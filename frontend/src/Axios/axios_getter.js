ab51996a78a60626982129f107f863239474585e
Victor Hua <vhua@princeton.edu>
2019-11-10 18:17:26 -0500

    first build file

--- frontend/src/Axios/axios_getter.js
+++ frontend/src/Axios/axios_getter.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const getOrderInfo = (attribute) => {
-  return axios.get('http://localhost:5000/customer/orderinfo')
+  return axios.get('http://localhost:8080/customer/orderinfo')
       .then(res => {
         return res.data[0][attribute];
       });
 }
 
 export const postMakeOrder = (update) => {
-  axios.post('http://localhost:5000/customer/makeorder', update)
+  axios.post('http://localhost:8080/customer/makeorder', update)
       .then(res => {
      });
 }
