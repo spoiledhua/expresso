330c9f77b43d5ab4a28a686e7847dd9ab638957d
Hari Raval <hraval@princeton.edu>
2019-11-20 12:15:20 -0500

    updated payment code

--- LDAP.py
+++ LDAP.py
@@ -27,11 +27,12 @@ class LDAP:
     def disconnect_LDAP(self):
         self._connection.unbind()
 
-    def get_all(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
-                                          attributes=['universityid', 'displayname', 'sn', 'givenname', 'mail'])
+    def get_all(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
+                                          attributes=['universityid', 'displayname', 'sn', 'givenname', 'mail',
+                                                      'pustatus', 'puclassyear'])
 
-        attributeName = ('universityid', 'displayname', 'sn', 'givenname', 'mail')
+        attributeName = ('displayname', 'sn', 'givenname', 'mail', 'pustatus', 'puclassyear')
         result = []
 
         if success:
@@ -42,8 +43,8 @@ class LDAP:
 
         return result
 
-    def get_id(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
+    def get_id(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
                                           attributes=['universityid'])
 
         result = []
@@ -55,8 +56,8 @@ class LDAP:
 
         return result
 
-    def get_displayname(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
+    def get_displayname(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
                                           attributes=['displayname'])
 
         result = []
@@ -68,8 +69,8 @@ class LDAP:
 
         return result
 
-    def get_givenname(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
+    def get_givenname(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
                                           attributes=['givenname'])
 
         result = []
@@ -81,8 +82,8 @@ class LDAP:
 
         return result
 
-    def get_sn(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
+    def get_sn(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
                                           attributes=['sn'])
 
         result = []
@@ -94,8 +95,8 @@ class LDAP:
 
         return result
 
-    def get_mail(self, puid):
-        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % puid,
+    def get_mail(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
                                           attributes=['mail'])
 
         result = []
@@ -107,14 +108,40 @@ class LDAP:
 
         return result
 
+    def get_pustatus(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
+                                          attributes=['pustatus'])
+
+        result = []
+
+        if success:
+            result.append(self._connection.entries[0]['pustatus'][0])
+        else:
+            print("Search failed", file=stderr)
+
+        return result
+
+    def get_puclassyear(self, netid):
+        success = self._connection.search('o=Princeton University,c=US', '(uid=%s)' % netid,
+                                          attributes=['puclassyear'])
+
+        result = []
+
+        if success:
+            result.append(self._connection.entries[0]['puclassyear'][0])
+        else:
+            print("Search failed", file=stderr)
+
+        return result
+
 
 def main():
     conn = LDAP()
     conn.connect_LDAP()
     searchResults = conn.get_all("jk30")
     print(searchResults)
-    searchResults = conn.get_id("jk30")
-    print(searchResults)
+    #searchResults = conn.get_id("jk30")
+    #print(searchResults)
     searchResults = conn.get_displayname("jk30")
     print(searchResults)
     searchResults = conn.get_givenname("jk30")
@@ -122,6 +149,11 @@ def main():
     searchResults = conn.get_sn("jk30")
     print(searchResults)
     searchResults = conn.get_mail("jk30")
+    print(searchResults)
+    searchResults = conn.get_pustatus("jk30")[0]
+    print(searchResults)
+    print(searchResults != 'undergraduate')
+    searchResults = conn.get_puclassyear("jk30")
     print(searchResults)
     conn.disconnect_LDAP()
 
