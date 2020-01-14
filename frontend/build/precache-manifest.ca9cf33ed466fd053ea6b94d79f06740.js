12e51eb53fb67b10488075c47ec2e039301afbf5
Victor Hua <vhua@princeton.edu>
2020-01-14 13:52:59 -0500

    final-commit-jan-14-other

---
+++ frontend/build/precache-manifest.ca9cf33ed466fd053ea6b94d79f06740.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab77e55c78ba2df6342609076995f1b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33eb65ae90d24f71dafd",
+    "url": "/static/css/2.5f2f5f0e.chunk.css"
+  },
+  {
+    "revision": "087d15be7f41872f25e6",
+    "url": "/static/css/main.b9c64099.chunk.css"
+  },
+  {
+    "revision": "33eb65ae90d24f71dafd",
+    "url": "/static/js/2.471566b2.chunk.js"
+  },
+  {
+    "revision": "087d15be7f41872f25e6",
+    "url": "/static/js/main.e19d06b2.chunk.js"
+  },
+  {
+    "revision": "c0fcfd10e0236f25d9f6",
+    "url": "/static/js/runtime-main.22990976.js"
+  },
+  {
+    "revision": "6236e0773b8862a590bf58c94116838d",
+    "url": "/static/media/animation.6236e077.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "e445ef56fd9de17940494ef821b2eac0",
+    "url": "/static/media/coffeesteam.e445ef56.gif"
+  },
+  {
+    "revision": "b08fa5629bc82d58da51a2a4c7be51d5",
+    "url": "/static/media/ding.b08fa562.mp3"
+  },
+  {
+    "revision": "e5a81cd5f049c61f25da6b61fd1fb7ff",
+    "url": "/static/media/dora.e5a81cd5.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "6cb48d5b257143d925b1a9d0288d481a",
+    "url": "/static/media/hari.6cb48d5b.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d21fdf75e959363b26640545d84e837",
+    "url": "/static/media/inventory.5d21fdf7.png"
+  },
+  {
+    "revision": "2ee4bb7f8be19070459dc36400b5a4d2",
+    "url": "/static/media/joseph.2ee4bb7f.jpg"
+  },
+  {
+    "revision": "3f0be33f3eb96363b58b47dff8c9ab2c",
+    "url": "/static/media/karen.3f0be33f.jpg"
+  },
+  {
+    "revision": "5a0a1c5ab5a5359c710f281b9472fc0f",
+    "url": "/static/media/logo.5a0a1c5a.png"
+  },
+  {
+    "revision": "edfb715e0b6df8735b592a278845ab0c",
+    "url": "/static/media/menu.edfb715e.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a96e95247cfab62cf380afc1c859e54a",
+    "url": "/static/media/victor.a96e9524.jpg"
+  }
+]);
